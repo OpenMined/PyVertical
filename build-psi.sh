ad89032a505d1db2204a8b9e537f06be0d86ffce
Robert Sandmann <r.sandmann@apheris.com>
2020-06-18 16:44:03 +0200

    add shell script to build psi python binding

---
+++ build-psi.sh
@@ -0,0 +1,2 @@
+bazel build @org_openmined_psi//private_set_intersection/python:private_set_intersection &&
+cp -f bazel-bin/external/org_openmined_psi/private_set_intersection/python/_psi_bindings.so src/psi/

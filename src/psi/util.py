a027e940bca0baf68061ff76ce3d77bf2d3be9c0
Robert Sandmann <r.sandmann@apheris.com>
2020-06-19 10:17:49 +0200

    remove reveal_intersection arg from compute_psi

--- src/psi/util.py
+++ src/psi/util.py
@@ -3,29 +3,23 @@
 from . import client, server
 
 
-def compute_psi(client_items, server_items, reveal_intersection=True, fpr=1e-9):
+def compute_psi(client_items, server_items, fpr=1e-9):
     """Compute the private set intersection of `client_items` and `server_items`.
 
     Args:
         client_items (List[str]) : The items provided by the client
         server_items (List[str]) : The items provided by the server
-        reveal_intersection (bool) : If True, return the common items as a list
         fpr (float) : The false positive ratio
 
     Returns:
         The cardinality (int) of the intersection set or the intersection
         set (List[str]) itself of client and server items
     """
-    c = client.CreateWithNewKey(reveal_intersection)
-    s = server.CreateWithNewKey(reveal_intersection)
+    c = client.CreateWithNewKey(True)
+    s = server.CreateWithNewKey(True)
 
     setup = s.CreateSetupMessage(fpr, len(client_items), server_items)
     request = c.CreateRequest(client_items)
     resp = s.ProcessRequest(request)
 
-    if reveal_intersection:
-        intersection = c.GetIntersection(setup, resp)
-    else:
-        intersection = c.GetIntersectionSize(setup, resp)
-
-    return intersection
+    return c.GetIntersection(setup, resp)

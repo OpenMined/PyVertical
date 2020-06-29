373fd39f289729ebdb50661d228e099faabe1fcf
pavlos-p <papadopoylos.paul@gmail.com>
2020-06-29 10:37:44 +0100

    a NewVerticalDataLoader which takes the dataset, creates 2 vertically-partitioned datasets and returns them

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,11 +1,14 @@
 """
 Vertically partitioned dataloader
 """
+import sys
+sys.path.append('../')
 from typing import List, Tuple
 from uuid import UUID
 
 from torch.utils.data import DataLoader
 from torch.utils.data._utils.collate import default_collate
+from src.dataset import partition_dataset
 
 
 def id_collate_fn(batch: Tuple) -> List:
@@ -56,6 +59,26 @@ class PartitionDistributingDataLoader:
 
         self.dataloader1 = VerticalDataLoader(dataset1, *args, **kwargs)
         self.dataloader2 = VerticalDataLoader(dataset2, *args, **kwargs)
+
+    def __iter__(self):
+        return zip(self.dataloader1, self.dataloader2)
+
+class NewVerticalDataLoader:
+    """Dataloader which batches data from a complete
+    set of vertically-partitioned datasets
+    i.e. the images dataset AND the labels dataset
+    """
+
+    def __init__(self, dataset, *args, **kwargs):
+        
+        # Split data
+        self.data_partition1, self.data_partition2 = partition_dataset(dataset, remove_data=False, keep_order=True)
+
+        assert self.data_partition1.targets is None
+        assert self.data_partition2.data is None
+
+        self.dataloader1 = VerticalDataLoader(self.data_partition1, *args, **kwargs)
+        self.dataloader2 = VerticalDataLoader(self.data_partition2, *args, **kwargs)
 
     def __iter__(self):
         return zip(self.dataloader1, self.dataloader2)

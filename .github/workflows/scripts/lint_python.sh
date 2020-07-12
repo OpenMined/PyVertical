d845278cbc98aa1f4c60128f5c20152bc20ede0c
Tom Titcombe <t.j.titcombe@gmail.com>
2020-07-12 15:02:10 +0100

    Move linting code to test.yml
    Makes it easier to see which line failed

--- .github/workflows/scripts/lint_python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-flake8 --config=.flake8 .
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=100 --statistics
-
-black --check src
-
-mypy src --ignore-missing-imports

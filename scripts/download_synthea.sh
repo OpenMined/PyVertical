1a659ebf17e9a77968deec199382b00598226f75
Tom Titcombe <t.j.titcombe@gmail.com>
2020-08-02 14:59:55 +0100

    Correct synthea download script for synthea submodule
    Add synthea info to readme

--- scripts/download_synthea.sh
+++ scripts/download_synthea.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#git clone https://github.com/synthetichealth/synthea.git
-cd synthea
-./run_synthea -s 42 -p 5 --exporter.csv.export true
-cd ..
-mv synthea/output/csv/*csv data/synthea/
 
-cd data/synthea
+# Generate data
+cd third-party/synthea
+./run_synthea -s 42 -p 50000 --exporter.csv.export true
+
+# Copy data to PyVertical
+mv output/csv/*csv ../../data/synthea/
+
+cd ../../data/synthea
 # Remove unnecessary files
 rm allergies.csv careplans.csv devices.csv encounters.csv imaging_studies.csv
 rm immunizations.csv organizations.csv payers.csv payer_transitions.csv

6bff92641d5d43c6517ed216d65310037a68b1c2
Robert Sandmann <r.sandmann@apheris.com>
2020-08-06 10:30:23 +0200

    add server and client class for psi

--- src/psi/util.py
+++ src/psi/util.py
@@ -3,6 +3,32 @@
 from . import client, server
 
 
+class Server:
+    def __init__(self, server_items, fpr=1e-9):
+        reveal_intersection = True
+        self._server = server.CreateWithNewKey(reveal_intersection)
+        self._items = server_items
+        self._fpr = fpr
+
+    def process_request(self, request, len_client_items):
+        setup = self._server.CreateSetupMessage(
+            self._fpr, len_client_items, self._items
+        )
+        response = self._server.ProcessRequest(request)
+        return setup, response
+
+
+class Client:
+    def __init__(self, client_items):
+        reveal_intersection = True
+        self._client = client.CreateWithNewKey(reveal_intersection)
+        self._items = client_items
+        self.request = self._client.CreateRequest(client_items)
+
+    def compute_intersection(self, setup, response):
+        return self._client.GetIntersection(setup, response)
+
+
 def compute_psi(client_items, server_items, fpr=1e-9):
     """Compute the private set intersection of `client_items` and `server_items`.
 

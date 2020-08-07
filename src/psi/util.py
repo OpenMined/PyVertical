5b7bf6c3e54429fb64f5a2a90f73bed3ef363cde
Robert Sandmann <r.sandmann@apheris.com>
2020-08-07 08:38:49 +0200

    remove deprecated compute_psi

--- src/psi/util.py
+++ src/psi/util.py
@@ -72,27 +72,3 @@ class Client:
             The intersection set (List[str]) of client and server items
         """
         return self._client.GetIntersection(setup, response)
-
-
-def compute_psi(client_items, server_items, fpr=1e-9):
-    """Compute the private set intersection of `client_items` and `server_items`.
-
-    Args:
-        client_items (List[str]) : The items provided by the client
-        server_items (List[str]) : The items provided by the server
-        fpr (float) : The false positive ratio
-
-    Returns:
-        The intersection set (List[str]) of client and server items
-    """
-    if len(client_items) == 0 or len(server_items) == 0:
-        return []
-
-    c = client.CreateWithNewKey(True)
-    s = server.CreateWithNewKey(True)
-
-    setup = s.CreateSetupMessage(fpr, len(client_items), server_items)
-    request = c.CreateRequest(client_items)
-    resp = s.ProcessRequest(request)
-
-    return c.GetIntersection(setup, resp)

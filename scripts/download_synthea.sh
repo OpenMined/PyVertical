42f0f183d9e7e70db90edee84232a5964538bda8
Tom Titcombe <t.j.titcombe@gmail.com>
2020-08-18 19:02:48 +0100

    Reduce number of patients to 5000

--- scripts/download_synthea.sh
+++ scripts/download_synthea.sh
@@ -2,13 +2,13 @@
 
 # Generate data
 cd third-party/synthea
-./run_synthea -s 42 -p 50000 --exporter.csv.export true
+./run_synthea -s 42 -p 5000 --exporter.csv.export true
 
 # Copy data to PyVertical
 mv output/csv/*csv ../../data/synthea/
 
 cd ../../data/synthea
 # Remove unnecessary files
-rm allergies.csv careplans.csv devices.csv encounters.csv imaging_studies.csv
-rm immunizations.csv organizations.csv payers.csv payer_transitions.csv
-rm procedures.csv providers.csv supplies.csv
+#rm allergies.csv careplans.csv devices.csv encounters.csv imaging_studies.csv
+#rm immunizations.csv organizations.csv payers.csv payer_transitions.csv
+#rm procedures.csv providers.csv supplies.csv

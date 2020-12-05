693a334a8e064b8bbabd348b39ff9f2f4b4831ad
Daniele Romanini <daniroma3@gmail.com>
2020-12-05 19:42:48 +0100

    first version of verticalFederatedDataLoader

--- examples/dualheaded/verticalfederateddataset.py
+++ examples/dualheaded/verticalfederateddataset.py
@@ -1,19 +1,26 @@
-import syft as sy
 from __future__ import print_function
+import syft as sy
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data._utils.collate import default_collate
 
 """
 Utility functions to split and distribute the data across different workers, 
 create vertical datasets and federate them. It also contains datasets and dataloader classes. 
 This code is meant to be used with dual-headed Neural Networks, where there are a bunch of different workers, 
 which agrees on the labels, and there is a server  with the labels only. 
-
 Code built upon: 
 - Abbas Ismail's (@abbas5253) work on dual-headed NN. In particular, check Configuration 1: 
   https://github.com/abbas5253/SplitNN-for-Vertically-Partitioned-Data/blob/master/Configuration_1.ipynb
 - Syft 2.0 Federated Learning dataset and dataloader: https://github.com/OpenMined/PySyft/tree/syft_0.2.x/syft/frameworks/torch/fl
 
+TODO: 
+    - replace ids with UUIDs
+    - there is a bug in creation of BaseDataset
+    - create class for splitting the data 
+    - create LabelSet and SampleSet (to accomodate later different roles of workers)
+    - improve DataLoader to accomodate different sampler (e.g. random sampler when shuffle) and different batch size
 """
 
 
@@ -21,19 +28,14 @@ def split_data(dataset, worker_list=None, n_workers=2, label_server=None):
     """
     Utility function to create a vertical split of the data. It also creates a numerical index to keep
     track of the single data across different split. 
-
     Args: 
         dataset: an iterable object represent the dataset. Each element of the iterable 
         is supposed to be a tuple of [tensor, label]. It could be an iterable "Dataset" object in PyTorch. 
         #TODO: add support for taking a Dataloader as input, to iterate over batches instead of single examples.
-
         worker_list (optional): The list of VirtualWorkers to distribute the data vertically across. 
-
         n_workers(optional, default=2): The number of workers to split the data across. If worker_list is not passed, this is necessary to create the split.
-
         label_server (optional): the server which owns only the labels (e.g. in a dual-headed NN setting)
         #TODO: add the code to send labels to the server
-
     Returns: 
         a dictionary holding as keys the workers passed as parameters, or integers corresponding to the split, 
         and as values a list of lists, where the first element are the single tensor of the data, the second the labels, 
@@ -83,16 +85,12 @@ def split_data(dataset, worker_list=None, n_workers=2, label_server=None):
 def split_data_create_vertical_dataset(dataset, worker_list, label_server=None): 
     """
     Utility function to distribute the data vertically across workers and create a vertical federated dataset.
-
     Args: 
         dataset: an iterable object represent the dataset. Each element of the iterable 
         is supposed to be a tuple of [tensor, label]. It could be an iterable "Dataset" object in PyTorch. 
         #TODO: add support for taking a Dataloader as input, to iterate over batches instead of single examples.
-
         worker_list: The list of VirtualWorkers to distribute the data vertically across. 
-
         label_server (optional): the server which owns only the labels (e.g. in a dual-headed NN setting)
-
     Returns: 
         a VerticalFederatedDataset.
     """    
@@ -111,25 +109,22 @@ def split_data_create_vertical_dataset(dataset, worker_list, label_server=None):
 class BaseVerticalDataset(Dataset): 
     """
     Base Vertical Dataset class, containing a portion of a vertically splitted dataset.
-
     Args: 
         datatuples: a list where each element is another list (or tuple) of exactly 3 elements.
         The first one is a sample data, the second one is the corresponding label, and the third one the index
         (necessary to keep track of the same vertically splitted examples across multiple workers)
-
         worker_id (optional): the worker to which we want to send the dataset
-
     """
     def __init__(self, datatuples, worker_id=None):
         
-        self._fill_tensors(datatuples)
+        self.__fill_tensors(datatuples)
             
         self.worker_id = None
         if worker_id != None: 
             self.send_to_worker(worker_id)
             self.worker_id = worker_id
             
-        self._dataset_tolist() 
+        self.__dataset_tolist() 
         
             
     def __len__(self):
@@ -138,14 +133,13 @@ class BaseVerticalDataset(Dataset):
         """
         return self.data_tensor.shape[0]
         
-    def __get_item__(self, idx):
+    def __getitem__(self, index):
         """
         Args: 
             idx: index of the example we want to get 
-
         Returns: a tuple with data, label, index of a single example.
         """
-        return tuple([self.data_tensor[idx], self.label_tensor[idx], self.index_tensor[idx]])
+        return tuple([self.data_tensor[index], self.label_tensor[index], self.index_tensor[index]])
         
     def __fill_tensors(self, data_tuples):
         """
@@ -161,17 +155,15 @@ class BaseVerticalDataset(Dataset):
         """
         list_dataset = []
         for i in range(0, self.__len__()):
-            list_dataset.append(self.__get_item__(i))
+            list_dataset.append(self.__getitem__(i))
         self.dataset = list_dataset
             
 
     def send_to_worker(self, worker):
         """
         Send the dataset to a worker.
-
         Args: 
             the worker to which we want to send the dataset 
-
         Returns: 
             pointers to the remote data, the labels and the index tensors
         """
@@ -191,7 +183,6 @@ class VerticalFederatedDataset():
     This serves as an input to VerticalFederatedDataLoader. 
     Same principle as in Syft 2.0 for FederatedDataset: 
     https://github.com/OpenMined/PySyft/blob/syft_0.2.x/syft/frameworks/torch/fl/dataset.py
-
     Args: 
         datasets: list of BaseVerticalDatasets.
     """
@@ -202,22 +193,73 @@ class VerticalFederatedDataset():
         for dataset in datasets:
             worker_id = dataset.worker_id
             self.datasets[worker_id] = dataset
+            
+        self.workers = self.__workers()
     
     
-    def workers(self):
+    def __workers(self):
         """
         Returns: list of workers
         """
         return list(self.datasets.keys())
-
-
- class VerticalFederatedDataLoader():
-    """
-    Data loader class, It combines a VerticalFederatedDataset and a sampler. 
-
-    TODO: implement the class. Check also FederatedDataLoader of Syft 2.0: 
-    https://github.com/OpenMined/PySyft/blob/syft_0.2.x/syft/frameworks/torch/fl/dataloader.py
-    """
     
-    def __init__(self, vertical_fed_dataset):
-    	self.vertical_fed_dataset = vertical_fed_dataset
+    def get_dataset(self, worker):
+        self[worker].federated = False
+        dataset = self[worker].get()
+        del self.datasets[worker]
+        return dataset
+
+    def __getitem__(self, worker_id):
+        """
+        Args:
+            worker_id[str,int]: ID of respective worker
+        Returns:
+            Get Datasets from the respective worker
+        """
+
+        return self.datasets[worker_id]
+
+    def __len__(self):
+
+        return sum(len(dataset) for dataset in self.datasets.values())
+
+    def __repr__(self):
+
+        fmt_str = "FederatedDataset\n"
+        fmt_str += f"    Distributed accross: {', '.join(str(x) for x in self.workers)}\n"
+        fmt_str += f"    Number of datapoints: {self.__len__()}\n"
+        return fmt_str
+    
+    
+class SinglePartitionDataLoader(DataLoader):
+    """DataLoader for a single vertically-partitioned dataset"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.collate_fn = default_collate
+        
+class VerticalFederatedDataLoader:
+    """Dataloader which batches data from a complete
+    set of vertically-partitioned datasets
+    """
+
+    def __init__(self, vf_dataset, batch_size=8, shuffle=False, *args, **kwargs):
+
+        self.vf_dataset = vf_dataset
+        
+        single_loaders_list = []
+        for d in vfd.datasets.values(): 
+            single_loaders_list.append(SinglePartitionDataLoader(d))
+            
+        self.workers = list(vf_dataset.keys())
+        
+
+    def __iter__(self):
+        return zip(*self.vf_dataset)
+
+    def __len__(self):
+        l = 0
+        for x in self.vf_dataset.datasets.values():
+            l += len(x)
+        return l // len(self.workers)
